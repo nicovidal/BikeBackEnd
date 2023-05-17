c7bccea316444c403929a1c23ff58e65ccb16710
Nicolas Vidal <nico.vidal@duocuc.cl>
2023-05-16 21:03:07 -0400

    update ok

--- controllers/alumnos.js
+++ controllers/alumnos.js
@@ -16,5 +16,81 @@ const getAlumnos=async(req,res=response)=>{
 
 };
 
+const actualizarAlumno=async(req,res=response)=>{
 
-module.exports = {getAlumnos};
+    const alumnoId=req.params.id;
+
+    const uid=req.uid
+
+    try {
+        const alumno=await Bike.findById(alumnoId);
+
+        if(!alumno){
+            return res.status(404).json({
+                ok:false,
+                msg:'Alumno no existe por ese id'
+            })
+        }
+
+        const nuevoAlumno={
+            ...req.body,
+            user:uid
+        }
+
+        const alumnoActualizado=await Bike.findByIdAndUpdate(alumnoId,nuevoAlumno,{new:true})
+
+        res.json({
+            ok:true,
+            alumno:alumnoActualizado
+        });
+
+
+
+        
+    } catch (error) {
+
+        console.log(error)
+        res.status(500).json({
+            ok:false,
+            msg:'Hable con el administrador'
+        })
+
+        
+    }
+}
+
+
+const eliminarAlumno=async(req,res=response)=>{
+
+    const alumnoId=req.params.id;
+/*     const uid=req.uid; */
+
+    try {
+
+        const alumno=await Bike.findById(alumnoId);
+
+        if(!alumno){
+            return res.status(400).json({
+                ok:false,
+                msg:'Alumno no existe'
+            })
+        }
+
+        await Bike.findByIdAndDelete(alumnoId);
+
+        res.json({ok:true})
+        
+    } catch (error) {
+
+        console.log(error)
+        res.status(500).json({
+            ok:false,
+            msg:'Hable con el administrador'
+        })
+        
+    }
+
+}
+
+
+module.exports = {getAlumnos,eliminarAlumno,actualizarAlumno};
--- routes/alumnos.js
+++ routes/alumnos.js
@@ -1,9 +1,17 @@
 const {Router}=require('express');
-const { getAlumnos } = require('../controllers/alumnos');
+const { getAlumnos, eliminarAlumno, actualizarAlumno } = require('../controllers/alumnos');
 const router=Router();
 
 
 router.get('/alumnos',getAlumnos);
 
+
+
+//borrar alumno
+
+
+router.put('/alumnos/:id',actualizarAlumno)
+
+router.delete('/alumnos/:id',eliminarAlumno)
 
 module.exports=router;
